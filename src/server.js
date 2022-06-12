6d3d273092932499e1021d7d6a1c47339824ed55
Lucas Pavao <lucaspavao89@gmail.com>
2022-06-12 17:55:06 -0300

    CRUD completo

--- src/server.js
+++ src/server.js
@@ -1,11 +1,43 @@
 const porta = 3003;
-
 const express = require("express");
+const bodyParser = require("body-parser");
+const BD = require("./BancoDeDados");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/produtos", (req, res, next) => {
-  res.send({ nome: "Notebook", preco: "123.45" });
+  res.send(BD.getProdutos());
+});
+
+app.get("/produtos/:id", (req, res, next) => {
+  res.send(BD.getProduto(req.params.id));
+});
+
+app.post("/produtos", (req, res, next) => {
+  const produto = BD.salvarProduto({
+    nome: req.body.nome,
+    preco: req.body.preco,
+  });
+
+  res.send(produto);
+});
+
+app.put("/produtos/:id", (req, res, next) => {
+  const produto = BD.salvarProduto({
+    id: req.params.id,
+    nome: req.body.nome,
+    preco: req.body.preco,
+  });
+
+  res.send(produto);
+});
+
+app.delete("/produtos/:id", (req, res, next) => {
+  const produto = BD.excluirProduto(req.params.id);
+
+  res.send(produto);
 });
 
 app.listen(porta, () => {
